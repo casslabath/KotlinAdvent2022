f1ab6e1f820ea5fcd4c90e74d16f21d575978afd
casslabath <casslabath@yahoo.com>
2022-12-01 15:18:20 -0800

    dont need semicolons

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,34 +1,34 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        var most = Int.MIN_VALUE;
-        var currentCals = 0;
+        var most = Int.MIN_VALUE
+        var currentCals = 0
         for(cal in input) {
             if(cal.isEmpty()) {
-                most = currentCals.coerceAtLeast(most);
-                currentCals = 0;
+                most = currentCals.coerceAtLeast(most)
+                currentCals = 0
             } else {
-                currentCals += Integer.parseInt(cal);
+                currentCals += Integer.parseInt(cal)
             }
         }
-        return most;
+        return most
     }
 
     fun part2(input: List<String>): Int {
-        var topThree = mutableListOf(0,0,0);
-        var currentCals = 0;
+        val topThree = mutableListOf(0,0,0)
+        var currentCals = 0
 
         for(cal in input) {
             if(cal.isEmpty()) {
                 if(currentCals > topThree[0]) {
-                    topThree[0] = currentCals;
-                    topThree.sort();
+                    topThree[0] = currentCals
+                    topThree.sort()
                 }
-                currentCals = 0;
+                currentCals = 0
             } else {
-                currentCals += Integer.parseInt(cal);
+                currentCals += Integer.parseInt(cal)
             }
         }
-        return topThree.sum();
+        return topThree.sum()
     }
 
     val input = readInput("Day01")
